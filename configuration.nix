3fea45f8d74e75bdf04374e1e55da5ec61e2dad7
helladecimal <heckadec@gmail.com>
2026-07-17 14:26:50 -0400

    change swaylock for hyprlock

--- configuration.nix
+++ configuration.nix
@@ -161,7 +161,7 @@
     gh
     fuzzel
     swaybg
-    swaylock
+    hyprlock
     waypaper
     busybox
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
