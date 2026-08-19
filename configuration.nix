1e4e5dc6685cfddd5bce7785e9e22cf590fbdf7f
helladecimal <heckadec@gmail.com>
2026-08-18 23:51:41 -0400

    fix config

--- configuration.nix
+++ configuration.nix
@@ -78,8 +78,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT=80
-      STOP_CHARGE_THRESH_BAT0=85
+      START_CHARGE_THRESH_BAT=80;
+      STOP_CHARGE_THRESH_BAT0=85;
     };
   };
 
