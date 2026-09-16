36157785541e640ee335b0c411956a2455820e55
helladecimal <heckadec@gmail.com>
2026-09-16 08:50:43 -0400

    Remove stuff

--- configuration.nix
+++ configuration.nix
@@ -84,8 +84,6 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  services.acpid.lidEventCommands = "hyprlock";
-
   # Enable Ly display manager
   services.displayManager.ly.enable = true;
 
@@ -130,14 +128,7 @@
   };
 
   programs.niri.enable = true;
-  programs.fht-compositor.enable = true;
   services.displayManager.defaultSession = "niri";
-
-  xdg.portal = {
-      enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-      configPackages = [ pkgs.xdg-desktop-portal-gtk ];
-  };
 
   # services.picom.enable = true;
 
@@ -224,7 +215,6 @@
     nerd-fonts.agave
     brightnessctl
 
-    moonlight-qt
     rustdesk
 
     vesktop
